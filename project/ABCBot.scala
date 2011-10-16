e8f1b3183309bd232a6412098baadb8f6ef7de37
Dirk Husemann <dr_who@d2h.net>
2011-10-16 11:55:22 +0200

    ~ git branch now showing (not perfect, really need to do a grep)

--- project/ABCBot.scala
+++ project/ABCBot.scala
@@ -28,8 +28,8 @@ object ShellPrompt {
         def buffer[T] (f: => T): T = f
     }
     def currBranch = (
-        ("git status -sb" lines_! devnull headOption)
-        getOrElse "-" stripPrefix "## "
+        ("git branch" lines_! devnull headOption)
+        getOrElse "-" stripPrefix "* "
     )
 
         val buildShellPrompt = { 
