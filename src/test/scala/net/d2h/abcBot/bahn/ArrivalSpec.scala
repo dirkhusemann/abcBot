b244430633f8d1b68150d8ebce423ebcba9a2b81
Dirk Husemann <dr_who@d2h.net>
2011-10-19 09:13:52 +0200

    + ArrivalSpec

---
+++ src/test/scala/net/d2h/abcBot/bahn/ArrivalSpec.scala
@@ -0,0 +1,44 @@
+// Copyright © 2011 Dirk Husemann. All Rights Reserved.
+
+// This file is part of ABCBot.
+// 
+// ABCBot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// ABCBot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with ABCBot.  If not, see <http://www.gnu.org/licenses/>.
+
+package net.d2h.abcBot.bahn {
+
+    import org.specs2._
+
+    class ArrivalSpec extends Specification { def is = 
+
+        "This is a specification to check the Arrival class" ^
+                                                             p^
+        "An Arrival object should"                           ^
+            "capture arrival time information"               ! arrival().isCaptured ^
+                                                             end
+
+
+        trait ArrivalSample { 
+            val arr = Arrival("00:00", "S", "S2", "Zürich", ViaStation("Thalwil", "23:23") :: ViaStation("Horgen", "23:28") :: Nil)            
+        }
+
+        case class arrival() extends ArrivalSample { 
+            def isCaptured =
+                (arr.time mustEqual "00:00") and 
+                (arr.trainType mustEqual "S") and 
+                (arr.train mustEqual "S2") and
+                (arr.sourceLocation mustEqual "Zürich") and
+                (arr.stations.size mustEqual 2)
+        }
+    }
+}
