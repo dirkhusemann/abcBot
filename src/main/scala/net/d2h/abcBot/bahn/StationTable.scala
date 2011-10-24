5f3a07d5de935c0525ce730c39ff308f6e2eec0d
dirk husemann <dirk@d2h.net>
2011-10-24 21:23:48 +0200

    ~ cleaning up ~ using joda time DateTime and LocalTime instead of strings for date and time stuff now

--- src/main/scala/net/d2h/abcBot/bahn/StationTable.scala
+++ src/main/scala/net/d2h/abcBot/bahn/StationTable.scala
@@ -19,6 +19,7 @@
 package net.d2h.abcBot.bahn { 
 
     import scala.collection.mutable.Map
+    import org.joda.time.{LocalTime, DateTime}
 
     /**
      * ViaStation captures stations listed for a connection.
@@ -26,66 +27,91 @@ package net.d2h.abcBot.bahn {
      * @param name String containing station name
      * @param time String containing listed departure time
      */
-    case class ViaStation(name: String, time: String)
+    case class ViaStation(name: String, time: LocalTime)
 
     /**
      * ArrivalDeparture is the abstract super class capturing an
      * arrival or departure line listed in a station table.
      * 
-     * @param adTime String containing arrival or departure time of
-     *               the train
-     * @param adTrainType String containg the train type of the train;
-     *                    for example, "IR", "IC", "ICE", "TGV"
-     * @param adTrain String containing the complete train identifier;
-     *                for example, "ICE 104"
-     * @param adSourceDestination String containing the original
-     *                            source or final destination of the
-     *                            train
-     * @param adStations List containing ViaStations
+     * @param time LocalTime object containing arrival or departure time of
+     *             the train
+     * @param trainType String containg the train type of the train;
+     *                  for example, "IR", "IC", "ICE", "TGV"
+     * @param train String containing the complete train identifier;
+     *              for example, "ICE 104"
+     * @param sourceDestination String containing the original
+     *                          source or final destination of the
+     *                          train
+     * @param stations List containing ViaStations
      */
-    abstract class ArrivalDeparture(val time: String, val trainType: String, val train: String, 
+    abstract class ArrivalDeparture(val time: LocalTime, val trainType: String, val train: String, 
                                     val sourceDestination: String, val stations: List[ViaStation])
     
     /**
      * Arrival captures an arrival line listed in a station table.
      * 
-     * @param time String containing arrival time of the train
-     * @param trainType String containg the train type of the train;
-     *                  for example, "IR", "IC", "ICE", "TGV"
-     * @param train String containing the complete train identifier;
-     *              for example, "ICE 104"
-     * @param sourceLocation String containing the original
+     * @param arrTime LocalTime containing arrival time of the train
+     * @param arrTrainType String containg the train type of the train;
+     *                     for example, "IR", "IC", "ICE", "TGV"
+     * @param arrTrain String containing the complete train identifier;
+     *                 for example, "ICE 104"
+     * @param arrSourceLocation String containing the original
      *                       source location of the train
-     * @param stations List containing ViaStations
+     * @param arrStations List containing ViaStations
      */
-    case class Arrival(arrTime: String, arrTrainType: String, arrTrain: String, sourceLocation: String, arrStations: List[ViaStation])
+    case class Arrival(arrTime: LocalTime, arrTrainType: String, arrTrain: String, sourceLocation: String, arrStations: List[ViaStation])
          extends ArrivalDeparture(arrTime, arrTrainType, arrTrain, sourceLocation, arrStations)
 
     /**
      * Departure captures a departure line listed in a station table.
      * 
-     * @param time String containing departure time of the train
-     * @param trainType String containg the train type of the train;
-     *                  for example, "IR", "IC", "ICE", "TGV"
-     * @param train String containing the complete train identifier;
-     *              for example, "ICE 104"
-     * @param destinationLocation String containing the final
-     *                            destination location of the train
-     * @param stations List containing ViaStations
+     * @param depTime LocalTime containing departure time of the train
+     * @param depTrainType String containg the train type of the train;
+     *                     for example, "IR", "IC", "ICE", "TGV"
+     * @param depTrain String containing the complete train identifier;
+     *                 for example, "ICE 104"
+     * @param depDestinationLocation String containing the final
+     *                               destination location of the train
+     * @param depStations List containing ViaStations
      */
-    case class Departure(depTime: String, depTrainType: String, depTrain: String, destinationLocation: String, depStations: List[ViaStation]) 
+    case class Departure(depTime: LocalTime, depTrainType: String, depTrain: String, destinationLocation: String, depStations: List[ViaStation]) 
          extends ArrivalDeparture(depTime, depTrainType, depTrain, destinationLocation, depStations)
 
 
-    class StationTable(station: String, date: String) { 
-        var arrivals: Map[String, List[Arrival]] = Map()
-        var departures: Map[String, List[Departure]] = Map()
+    /**
+     * StationTable class for capturing departure and arrival
+     * information for a station.
+     * @param station String containing the station name
+     * @param date DateTime object containing the date for which to
+     *             retrieve the station table
+     */
+    class StationTable(station: String, date: DateTime) { 
+        /**
+         * Map containing LocalTime -> List[Arrival] mappings which
+         * each list containing the arrivals for a given time.
+         */
+        var arrivals: Map[LocalTime, List[Arrival]] = Map()
+        /**
+         * Map containing LocalTime -> List[Departure] mappings which
+         * each list containing the departures for a given time.
+         */
+        var departures: Map[LocalTime, List[Departure]] = Map()
 
+        /**
+         * Add an Arrival to the StationTable.
+         * @param arrival Arrival object to add
+         * @return reference to self for chaining
+         */
         def +=(arrival: Arrival): StationTable = { 
             arrivals(arrival.time) = arrival :: arrivals.getOrElse(arrival.time, Nil) 
             this
         }
 
+        /**
+         * Add a Departure to the StationTable.
+         * @param departure Departure object to add
+         * @return reference to self for chaining
+         */
         def +=(departure: Departure): StationTable = { 
             departures(departure.time) = departure :: departures.getOrElse(departure.time, Nil)
             this
--- src/test/scala/net/d2h/abcBot/bahn/ArrivalSpec.scala
+++ src/test/scala/net/d2h/abcBot/bahn/ArrivalSpec.scala
@@ -17,6 +17,8 @@
 
 package net.d2h.abcBot.bahn {
 
+    import org.joda.time.LocalTime
+
     import org.specs2._
 
     class ArrivalSpec extends Specification { def is = 
@@ -29,12 +31,12 @@ package net.d2h.abcBot.bahn {
 
 
         trait ArrivalSample { 
-            val arr = Arrival("00:00", "S", "S2", "Zürich", ViaStation("Thalwil", "23:23") :: ViaStation("Horgen", "23:28") :: Nil)            
+            val arr = Arrival(new LocalTime("00:00"), "S", "S2", "Zürich", ViaStation("Thalwil", new LocalTime("23:23")) :: ViaStation("Horgen", new LocalTime("23:28")) :: Nil)            
         }
 
         case class arrival() extends ArrivalSample { 
             def isCaptured =
-                (arr.time mustEqual "00:00") and 
+                (arr.time mustEqual new LocalTime("00:00")) and 
                 (arr.trainType mustEqual "S") and 
                 (arr.train mustEqual "S2") and
                 (arr.sourceLocation mustEqual "Zürich") and
@@ -52,13 +54,13 @@ package net.d2h.abcBot.bahn {
 
 
         trait DepartureSample { 
-            val dep = Departure("00:00", "S", "S2", "Pfäffikon SZ", ViaStation("Thalwil", "00:25") :: ViaStation("Horgen", "00:28") :: 
-                                ViaStation("Wädenswil", "00:32") :: Nil)            
+            val dep = Departure(new LocalTime("00:00"), "S", "S2", "Pfäffikon SZ", ViaStation("Thalwil", new LocalTime("00:25")) :: ViaStation("Horgen", new LocalTime("00:28")) :: 
+                                ViaStation("Wädenswil", new LocalTime("00:32")) :: Nil)            
         }
 
         case class departure() extends DepartureSample { 
             def isCaptured =
-                (dep.time mustEqual "00:00") and 
+                (dep.time mustEqual new LocalTime("00:00")) and 
                 (dep.trainType mustEqual "S") and 
                 (dep.train mustEqual "S2") and
                 (dep.destinationLocation mustEqual "Pfäffikon SZ") and
